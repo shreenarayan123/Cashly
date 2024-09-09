6d80bed15f19f4709aa47e56e4fc67386d679104
Your Name <127131074+shreenarayan123@users.noreply.github.com>
2024-09-09 16:31:31 +0530

    fixing imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import {Signup} from './pages/Signup';
 import Signin from './pages/Signin';
 import Dashboard from './pages/Dashboard';
@@ -7,7 +7,7 @@ import {SendMoney} from './pages/SendMoney';
 import SidebarHome from './components/SidebarHome';
 import History from './components/History';
 import Settings from './components/Settings';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
